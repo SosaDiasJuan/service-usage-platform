36281111035fcb4e5f934297991fe25fb5c75193
Sosa Dias Mena <f152665@net.in.iantel.com.uy>
2026-01-16 10:06:21 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/juanchi/usage/UsagePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.juanchi.usage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsagePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsagePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanchi/usage/UsagePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanchi.usage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsagePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
